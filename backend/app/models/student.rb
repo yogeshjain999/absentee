0bdd55ab0374cd2ffa7c6fc62dffbdf60d7eb3c6
priyanka216668 <priyanka@joshsoftware.com>
2019-04-26 23:26:42 +0530

    Added attendance association in student

--- backend/app/models/student.rb
+++ backend/app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
   translates :name
   globalize_accessors :locales => [:en, :'mr-IN'], :attributes => [:name]
+  has_many :attendances, class_name: "Attendance"
 end
