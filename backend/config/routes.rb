ddb1979bd0e1630579b3c104b0eb6a0227c57e91
priyanka216668 <priyanka@joshsoftware.com>
2019-04-27 00:53:15 +0530

    Defined routes for attendance api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :staffs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :attendances, only: [:create]
+  end
 end
