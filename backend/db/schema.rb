fb7b52c54c0889bfd1ff96500a1b65d9e3311cad
priyanka216668 <priyanka@joshsoftware.com>
2019-04-26 22:44:48 +0530

    Modified schema.rb

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,18 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_160652) do
+ActiveRecord::Schema.define(version: 2019_04_26_165342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "staffs", force: :cascade do |t|
+    t.string "mobile_number", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "staff_id", null: false
     t.string "name"
-    t.string "mobile_number"
     t.string "designation"
-    t.string "staff_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["mobile_number"], name: "index_staffs_on_mobile_number", unique: true
+    t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
   end
 
   create_table "standards", force: :cascade do |t|
