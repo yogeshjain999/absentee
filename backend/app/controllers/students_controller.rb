88b154bae1a0e00a60b0dade021b725e017b473e
priyanka216668 <priyanka@joshsoftware.com>
2019-04-27 08:31:04 +0530

    Added student datatable

--- backend/app/controllers/students_controller.rb
+++ backend/app/controllers/students_controller.rb
@@ -2,5 +2,9 @@ class StudentsController < ApplicationController
   def index
     @standards = Standard.all
     @pagy, @records = pagy(Student.all)
+    respond_to do |format|
+      format.html
+      format.json { render json: StudentDatatable.new(view_context) }
+    end
   end
 end
