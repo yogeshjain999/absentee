447f63c2a4883153c1183068f36dec4ffa6b3180
Akshay Birajdar <akshayb@joshsoftware.com>
2019-04-27 16:15:06 +0530

    Solve view instance

--- backend/app/controllers/dashboard_controller.rb
+++ backend/app/controllers/dashboard_controller.rb
@@ -3,7 +3,19 @@ class DashboardController < ApplicationController
     data = Attendance.where(school_id: 1).where('"date" > ?', Date.today.beginning_of_day).pluck(:present)
     result = data.uniq.map{|key| {key.to_s => (data.count(key)/data.size.to_f)*100}}
     @graph_data = [{name: "Present Students", y: result.map{|a| p a['true']}.compact.first},
-                   {name: "Absent Students",  y: result.map{|a| p a['false']}.compact.first}].to_json
+                   {name: "Absent Students",  y: result.map{|a| p a['false']}.compact.first}].to_s.to_json
+    # @graph_data = {
+    #   data: [
+    #     {
+    #       id: 'hsd',
+    #       value: 'sds'
+    #     },
+    #     {
+    #       id: 'hsdsd',
+    #       value: 'sdsdss'
+    #     }
+    #   ]
+    # }.to_s.to_json
     respond_to do |format|
       format.html { render 'index'}
       format.json { render json: @graph_data }
