90d9aa8ca62db729f63f66dcbc4f44de0cf51d0b
Akshay Birajdar <akshayb@joshsoftware.com>
2019-04-27 10:07:56 +0530

    Set sidekiq as Activejob adaptor

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -17,7 +17,9 @@ module Backend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.active_job.queue_adapter = :sidekiq
     config.time_zone = 'Kolkata'
+
     config.generators do |g|
       g.factory_bot false
       g.test_framework :rspec
