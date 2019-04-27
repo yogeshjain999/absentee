e5c8f25de7f2b771efc5bf77b6effeef374868f1
Akshay Birajdar <akshayb@joshsoftware.com>
2019-04-27 07:00:25 +0530

    Add translation

--- backend/app/assets/javascripts/standards.coffee
+++ backend/app/assets/javascripts/standards.coffee
@@ -3,4 +3,7 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready ->
-  $('.standard-select').select2()
+  $('.standard-select').select2({
+    dropdownAutoWidth: 'true',
+    width: '100%'
+  })
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 2019_04_26_203209) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "auth_token"
     t.bigint "school_id"
+    t.string "auth_token"
     t.index ["mobile_number"], name: "index_staffs_on_mobile_number", unique: true
     t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
     t.index ["school_id"], name: "index_staffs_on_school_id"
@@ -88,8 +88,8 @@ ActiveRecord::Schema.define(version: 2019_04_26_203209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "school_id"
-    t.bigint "standard_id"
     t.string "address"
+    t.bigint "standard_id"
     t.index ["school_id"], name: "index_students_on_school_id"
     t.index ["standard_id"], name: "index_students_on_standard_id"
   end
